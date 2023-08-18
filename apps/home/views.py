a173bce32d300f15f1de25cdf44e69430c05f6ee
anhndapec <129018473+anhndapec@users.noreply.github.com>
2023-08-18 09:03:14 +0700

    Update views.py

--- apps/home/views.py
+++ apps/home/views.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from datetime import datetime
 from django import template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.db.models import Q
@@ -219,7 +219,7 @@ class Tatcachuyendi(APIView):
                     #         'status': False, 
                     #         'error' : "sync chuyen di err"
                     #     })
-        return JsonResponse(results)
+        return Response(queryset)
         # except Exception as ex:
         #     print(ex)
         #     return Response({
