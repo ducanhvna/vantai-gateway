4e45d496b4db205077ade0c2c955b7de1999d669
DucAnh <ducanhvna@outlook.com>
2024-03-31 23:33:56 +0700

    update urls

--- apps/api_apec/urls.py
+++ apps/api_apec/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
 from .views import SyncUserDevice
 urlpatterns = [
-    path("api/apec/syncuser/", SyncUserDevice.as_view(), name='apec_syncuser'),
+    path("apec/api/syncuser/", SyncUserDevice.as_view(), name='apec_syncuser'),
 ]
--- apps/members/urls.py
+++ apps/members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
 from .views import CompanyViewSet
 urlpatterns = [
-    path("api/versions/", CompanyViewSet.as_view(), name='api_version'),
+    path("common/api/versions/", CompanyViewSet.as_view(), name='api_version'),
 ]
