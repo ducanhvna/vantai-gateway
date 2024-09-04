a1a430b8763d99c658e78b7e0796816302ec8d3c
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-09-04 16:16:23 +0700

    update template

--- addons/fleet_trip/models/fleet_trip.py
+++ addons/fleet_trip/models/fleet_trip.py
@@ -461,7 +461,23 @@ class FleetTrip(models.Model):
         ws1.cell(row=14, column=1).value = f"Số người:{self.number_people} người."
         ws1.cell(row=14, column=7).value = f"Số tấn HH:{self.product_weigh} tấn."
         
-       
+        
+        if (self.start_date):
+            ws1.cell(row=12, column=5).value = f"Từ : {self.start_date.hour}."
+            ws1.merge_cells(start_row=12, start_column=5, end_row=12, end_column=6) 
+            if self.start_date.minute > 0:
+                ws1.cell(row=12, column=7).value = f" {self.start_date.minute}."
+            ws1.cell(row=12, column=10).value = f" {self.start_date.day}."
+            ws1.cell(row=12, column=12).value = f" {self.start_date.month}."
+        if (self.end_date):
+            ws1.cell(row=13, column=5).value = f"Đến: {self.end_date.hour}."
+            ws1.merge_cells(start_row=13, start_column=5, end_row=13, end_column=6) 
+            if self.start_date.minute > 0:
+                ws1.cell(row=13, column=7).value = f" {self.end_date.minute}."
+            ws1.cell(row=13, column=10).value = f" {self.end_date.day}."
+            ws1.cell(row=13, column=12).value = f" {self.end_date.month}."
+            
+           
 
         # Assuming you have a record (template) with an image field (image_1920)
         if self.employee_plan_id:
