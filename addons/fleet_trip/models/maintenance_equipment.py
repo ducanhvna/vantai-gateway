dcb71c837688fb3fb178e11ad9fa664134d361d9
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-09-24 13:04:01 +0700

    Update maintenance_equipment.py

--- addons/fleet_trip/models/maintenance_equipment.py
+++ addons/fleet_trip/models/maintenance_equipment.py
@@ -11,8 +11,8 @@ class MaintenanceEquipment(models.Model):
 
     license_plate = fields.Char(related='vehicle_id.license_plate', string='Biển số', required=False)
     vehicle_id = fields.Many2one('fleet.vehicle', string='Phương tiện')
-    currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
-    model_id = fields.Many2one('vehicle_id.model_id', string="Nhãn xe")
+    department_belong_id = fields.Many2one('hr.department', string="Thuộc đơn vị")
+    model_id = fields.Many2one('vihicle_type',related='vehicle_id.model_id', string="Nhãn xe")
     fuel_id = fields.Many2one('product.template', string='Nhiên liệu')
     number_seat = fields.Integer('Số ghế')
     quota = fields.Integer('Định mức')
