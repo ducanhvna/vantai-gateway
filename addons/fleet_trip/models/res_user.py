2013140ac50e3c69845e0740481de59944061755
anhndapec <129018473+anhndapec@users.noreply.github.com>
2024-11-22 20:24:22 +0700

    Update res_user.py

--- addons/fleet_trip/models/res_user.py
+++ addons/fleet_trip/models/res_user.py
@@ -124,7 +124,7 @@ class HrEmployee(models.Model):
         for employee in self:
             employee.is_department_manager = bool(employee.department_manager_ids)
             if (employee.job_id != False):
-                employee.is_department_manager = employee.is_department_manager or employee.job_id.is_manage_department
+                employee.is_department_manager = (employee.is_department_manager) or bool(employee.job_id.is_manage_department)
         
             
     @api.depends("payroll_ids")
